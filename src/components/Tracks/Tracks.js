56ec913aefe3063adacf0e0cdcbb13794f88b295
Moritz <hello@moritzkuhn.com>
2023-10-11 20:30:06 +0200

    removing test comment

--- src/components/Tracks/Tracks.js
+++ src/components/Tracks/Tracks.js
@@ -4,7 +4,7 @@ import { useSearch } from '../SearchContext/SearchContext';
 export default function Tracks() {
     const { tracks, removeTrack } = useSearch();
     const [name, setName] = useState('Your Playlist')
-    // test
+    
     return (
         <>  
             <input style={{fontSize: '1.5rem'}} type="text" value={name} onChange={e => setName(e.target.value)}></input>
