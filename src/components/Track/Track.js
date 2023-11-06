187275775629f1dde83ece4934f4ef10ded0d769
Moritz <hello@moritzkuhn.com>
2023-11-07 00:05:51 +0100

    Refactor Track component to use useRef for track image and playPreview function

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -1,85 +1,111 @@
-import React, { useState } from "react";
+import React, { useRef } from "react";
 import "./Track.css";
-import 'react-tooltip/dist/react-tooltip.css'
-import { Tooltip } from 'react-tooltip'
-
+import 'react-tooltip/dist/react-tooltip.css';
+import { Tooltip } from 'react-tooltip';
 
 export default function Track(props) {
   const { track, onAddTrack, onRemoveTrack, isRemoval, wasAdded } = props;
-
-  const onHandleAddTrack = (track) => {
-    onAddTrack(track);
-  }
-
-  const trackPreview = new Audio(track.preview);
+  
+  const trackImageRef = useRef(null);
 
   const playPreview = () => {
-    var element = document.querySelector('.trackImage');
-    trackPreview.play();
-
-    if (trackPreview.duration > 0 && !trackPreview.paused) {
+    const trackPreview = new Audio(track.preview);
+    const element = trackImageRef.current;
+  
+    trackPreview.play().then(() => {
+      // Promise resolved, means playback has started
       element.style.border = "solid 2px red";
       console.log('playing');
-    } else {
+    }).catch(error => {
+      // Auto-play was prevented
+      console.log('playback prevented', error);
+    });
+  
+    trackPreview.addEventListener('ended', () => {
       element.style.border = "none";
-      console.log('not playing');
-    }
-
-  }
-
-  trackPreview.addEventListener('ended', () => {
-    var element = document.querySelector('.trackImage');
-    element.style.border = "none";
-    console.log('done playing');
-  });
+      console.log('done playing');
+    });
+  
+    trackPreview.addEventListener('pause', () => {
+      element.style.border = "none";
+      console.log('paused');
+    });
+  };
+  
 
   return (
     <>
       {isRemoval ? (
         <div className="track">
           <div className="trackInformation">
-            <img className="trackImage" src={track.image} alt={track.name + ' album cover'} />
+            <img
+              ref={trackImageRef}
+              className="trackImage"
+              src={track.image}
+              alt={`${track.name} album cover`}
+            />
             <div className="trackInformationText">
               <h3 className="trackName">{track.name}</h3>
               <p className="trackArtist">{track.artist} · {track.album}</p>
-              
             </div>
           </div>
-          <button data-tooltip-id="minusIcon-tooltip" data-tooltip-content="Remove track" className="trackAction" onClick={() => onRemoveTrack(track)}><div className="Icon" id="minusIcon"></div></button>
+          <button 
+            data-tooltip-id="minusIcon-tooltip" 
+            data-tooltip-content="Remove track" 
+            className="trackAction" 
+            onClick={() => onRemoveTrack(track)}>
+              <div className="Icon" id="minusIcon"></div>
+          </button>
           <Tooltip className="tooltip" id="minusIcon-tooltip" />
-
+        </div>
+      ) : wasAdded ? (
+        <div className="track">
+          <div className="trackInformation">
+            <img
+              ref={trackImageRef}
+              className="trackImage added"
+              src={track.image}
+              alt={`${track.name} album cover`}
+            />
+            <div className="trackInformationText">
+              <h3 className="trackName">{track.name}</h3>
+              <p className="trackArtist">{track.artist} · {track.album}</p>
+            </div>
+          </div>
+          <button 
+            disabled 
+            data-tooltip-id="checkIcon-tooltip" 
+            data-tooltip-content="Already added"
+            className="trackAction">
+              <div className="Icon" id="checkIcon"></div>
+          </button>
+          <Tooltip className="tooltip" id="checkIcon-tooltip" />
         </div>
       ) : (
-        wasAdded ? (
-          // track has already been added to the playlist
-          <div className="track">
-            <div className="trackInformation">
-              <img className="trackImage added" src={track.image} alt={track.name + ' album cover'} />
-              <div className="trackInformationText">
-                <h3 className="trackName">{track.name}</h3>
-                <p className="trackArtist">{track.artist} · {track.album}</p>
-              </div>
+        <div className="track">
+          <div className="trackInformation">
+            <img
+              onClick={playPreview}
+              ref={trackImageRef}
+              className="trackImage"
+              src={track.image}
+              alt={`${track.name} album cover`}
+            />
+            <div className="trackInformationText">
+              <h3 className="trackName">{track.name}</h3>
+              <p className="trackArtist">{track.artist} · {track.album}</p>
             </div>
-            <button disabled data-tooltip-id="checkIcon-tooltip" data-tooltip-content="Already added"
-              className="trackAction" ><div className="Icon" id="checkIcon"></div></button>
-            <Tooltip className="tooltip" id="checkIcon-tooltip" />
           </div>
-        ) : (
-          // track has not yet been added to the playlist
-          <div className="track">
-            <div className="trackInformation">
-              <img onClick={playPreview} className="trackImage" src={track.image} alt={track.name + ' album cover'} />
-              <div className="trackInformationText">
-                <h3 className="trackName">{track.name}</h3>
-                <p className="trackArtist">{track.artist} · {track.album}</p>
-              </div>
-            </div>
-            <button data-tooltip-id="plusIcon-tooltip" data-tooltip-content="Add track" className="trackAction" onClick={() => onHandleAddTrack(track)}><div className="Icon" id="plusIcon"></div></button>
-            <Tooltip className="tooltip" id="plusIcon-tooltip" />
-          </div>
-        )
+          <button 
+            data-tooltip-id="plusIcon-tooltip" 
+            data-tooltip-content="Add track" 
+            className="trackAction" 
+            onClick={() => onAddTrack(track)}>
+              <div className="Icon" id="plusIcon"></div>
+          </button>
+          <Tooltip className="tooltip" id="plusIcon-tooltip" />
+        </div>
       )}
     </>
-
   );
 }
